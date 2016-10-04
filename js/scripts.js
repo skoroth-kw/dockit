32a80f109b9ed4faf7385bc263865dd35b81c60d
Sajil Koroth <skoroth@learnvest.com>
2016-10-04 17:37:59 -0400

    Adding elastic search

--- js/scripts.js
+++ js/scripts.js
@@ -88,7 +88,7 @@ if(document.getElementById("search-field")){
 		if (event.keyCode == 13) {
 			// alert(document.getElementById('search-field').value);
 			 var el =  document.getElementById('main-document-content');
-			 el.parentNode.removeChild(el);
+			 //el.parentNode.removeChild(el);
 			 var iframe = document.createElement('iframe');
 			 var searchUrl = 'http://localhost:1337/elasticsearch/search?search='+document.getElementById('search-field').value;
 			 iframe.src = searchUrl;
