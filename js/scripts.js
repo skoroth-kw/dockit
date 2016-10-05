872b4004d9d06d38e894178ed8b6cef1ff408cdf
Sajil Koroth <skoroth@learnvest.com>
2016-10-05 16:57:30 -0400

    Adding elastic search

--- js/scripts.js
+++ js/scripts.js
@@ -96,16 +96,16 @@ if(document.getElementById("search-field")){
                var content = "";
 			    if (result.pages.length < 1){
 			      content+="<div><ul>";
-			      content+="<div style='font-size:22px;color:blue!important;padding-bottom:5px;text-align:center;padding-top:50px;'> No results found</div>";
+			      content+="<div style='font-size:1.0em;color:#4183c4!important;padding-bottom:5px;text-align:center;padding-top:50px;'> No results found</div>";
 			      content+=" </ul> </div>";
 			     }
 		        $.each(result.pages,function(index,page){
 		        	//alert(page);
 		        	content+="<div><ul>";
-					content+="<div style='font-size:22px;color:blue!important;padding-top:5px;padding-bottom:5px;'><a href='"+page.url+"' target='_blank'>["+page.name+"/"+page.version+"]</a></div>";
-					content+="<div style='font-size:12px;color:blue!important;padding-bottom:5px;'>[Confidence: "+page.score+"] [Created: "+page.date+"]</div>";
-					content+="<div style='font-size:18px;color:blue!important;padding-bottom:5px;'> <a href='"+page.url+"' target='_blank'>"+page.url+"</a></div>";
-	                content+="<div style='font-size:14px;color:gray;padding-bottom:20px;font-style:italics;'>"+page.highlighted+"</div>";
+					content+="<div style='font-size:.9em;color:#4183c4!important;padding-top:5px;padding-bottom:5px;'><a href='"+page.url+"' target='_blank'>["+page.name+"/"+page.version+"]</a></div>";
+					content+="<div style='font-size:.6em;color:#4183c4!important;padding-bottom:5px;'>[Confidence: "+page.score+"] [Created: "+page.date+"]</div>";
+					content+="<div style='font-size:.9em;color:#4183c4!important;padding-bottom:5px;'> <a href='"+page.url+"' target='_blank'>"+page.url+"</a></div>";
+	                content+="<div style='font-size:.9em;color:gray;padding-bottom:20px;font-style:italics;'>"+page.highlighted+"</div>";
 	                content+="</ul> </div>";
             	});
             	el.innerHTML = (content);
